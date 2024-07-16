bd8a978284747cd7bd03ef507744f9676b984bc7
lazuardh <habiyanlazuard21@gmail.com>
2024-07-16 23:53:59 +0700

    delete(screen): delete screen useless

--- lib/features/story/presentation/page/story_detail_page.dart
+++ lib/features/story/presentation/page/story_detail_page.dart
@@ -19,6 +19,18 @@ class _StoryDetailPageState extends State<StoryDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.red,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        title: Text(
+          "Detail Story",
+          style: AppTextStyle.bold.copyWith(
+            color: AppColors.white,
+            fontSize: AppFontSize.large,
+          ),
+        ),
+      ),
       body: SafeArea(
         child: ChangeNotifierProvider<DetailStoryProvider>(
           create: (context) => DetailStoryProvider(
@@ -39,7 +51,9 @@ class _StoryDetailPageState extends State<StoryDetailPage> {
                   child: Text(provider.message),
                 );
               } else if (provider.state == StoryState.loaded) {
-                return _StoryDetailWrapper(story: provider.detailStory);
+                return _StoryDetailWrapper(
+                  story: provider.detailStory,
+                );
               } else {
                 return Center(child: Text(provider.message));
               }
@@ -70,35 +84,6 @@ class _StoryDetailWrapper extends StatelessWidget {
           borderRadius: 0,
           fit: BoxFit.cover,
           isNetworkImage: true,
-        ),
-        Container(
-          color: const Color.fromARGB(36, 255, 255, 255),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              IconButton(
-                onPressed: () => Navigator.pop(context),
-                icon: const Icon(
-                  Icons.arrow_back_ios_new,
-                  color: Color.fromARGB(162, 242, 120, 107),
-                ),
-              ),
-              Text(
-                "Detail Story",
-                style: AppTextStyle.bold.copyWith(
-                  color: const Color.fromARGB(162, 242, 120, 107),
-                  fontSize: AppFontSize.large,
-                ),
-              ),
-              IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  Icons.bookmark,
-                  color: Color.fromARGB(162, 242, 120, 107),
-                ),
-              ),
-            ],
-          ),
         ),
         DraggableScrollableSheet(
           initialChildSize: 0.7,
